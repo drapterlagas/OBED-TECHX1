1af6383fd42c96d423766dd7e147a9b9c8081bcc
Obed tech <obedomoke18@gmail.com>
2025-04-08 22:09:41 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -234,7 +234,7 @@ const menu = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363398040175935@newsletter',
+          newsletterJid: '120363391979107532@newsletter',
           newsletterName: "ObedTechX",
           serverMessageId: 143
         }
